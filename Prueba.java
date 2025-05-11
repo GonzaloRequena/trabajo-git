5af644cd9badbb6ab0e870bf92099af4084ac907
Gonzalo Requena <gonzalo.re8@gmail.com>
2025-05-11 16:32:00 +0200

    Añadiendo recetas, archivo Java y modificando README.

---
+++ Prueba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	      System.out.println("¡Hola, mundo desde Git!");
+	      System.out.println("¿Cómo te llamas?");
+	      String nombre = sc.nextLine();
+	      System.out.println("¡Encantado, " + nombre + "!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # trabajo-git
+
+Este es un proyecto de ejemplo para aprender a usar Git.
+
+Contenido actual:
+- Un directorio con recetas (con una receta de tortilla).
+- Un archivo con código Java de ejemplo.
